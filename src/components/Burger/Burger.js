2309c1bee7fed90ddf7684abc1e38b26046dbf47
HuynhHongAn <huynhhongan2908@gmail.com>
2019-06-24 15:16:32 +0700

    Add and Remove Ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,13 +3,20 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-  const transformedIngredients = Object.keys(props.ingredients)
+  let transformedIngredients = Object.keys(props.ingredients)
     .map(igKey => { //This is the value of keys, i.e: meat, cheese, salad and bacon.
       return [...Array(props.ingredients[igKey])].map((_, i) => { 
         //Array(3): Create an array with 3 empty object
         return <BurgerIngredient key={igKey + i} type={igKey}></BurgerIngredient>
       });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
     });
+
+  if (transformedIngredients.length === 0){
+    transformedIngredients = <p>Please start adding ingredients!</p>
+  }
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top"/>

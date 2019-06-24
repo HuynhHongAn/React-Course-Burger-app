a25714f5f210443fd7a2a413ea79cba4112f2fd9
HuynhHongAn <huynhhongan2908@gmail.com>
2019-06-24 23:44:24 +0700

    Hide SideDrawer

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -3,18 +3,30 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.css';
+import BackDrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Aux';
 
 const sideDrawer = (props) => {
   //...
+  let attachdClasses = [classes.SideDrawer, classes.Close];
+  if (props.open){
+    attachdClasses = [classes.SideDrawer, classes.Open];
+  }
   return (
-    <div className={classes.SideDrawer}>
-      <div className={classes.Logo}>
-        <Logo/>
+    <Aux>
+      <BackDrop 
+        show={props.open} 
+        clicked={props.closed}/>
+      <div className={attachdClasses.join(' ')}>
+        <div className={classes.Logo}>
+          <Logo/>
+        </div>
+        <nav>
+          <NavigationItems></NavigationItems>
+        </nav>
       </div>
-      <nav>
-        <NavigationItems></NavigationItems>
-      </nav>
-    </div>
+    </Aux>
+    
   );
 }
 
